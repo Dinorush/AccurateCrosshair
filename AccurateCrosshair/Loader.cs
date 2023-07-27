ebb47e72033eeebe43450382fdf910554b8f9cdd
Dinorush <onparnorthstar@gmail.com>
2023-07-27 03:02:33 -0400

    Better practice for logger code

--- AccurateCrosshair/Loader.cs
+++ AccurateCrosshair/Loader.cs
@@ -5,20 +5,24 @@ using HarmonyLib;
 
 namespace AccurateCrosshair
 {
-    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.0.0")]
+    [BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.0.2")]
     internal class Loader : BasePlugin
     {
         public const string MODNAME = "AccurateCrosshair";
 
+#if DEBUG
         public static ManualLogSource Logger;
+#endif
 
         public override void Load()
         {
-            Logger = Log;
-            Logger.LogMessage("Loading " + MODNAME);
+#if DEBUG
+        `   Logger = Log;
+#endif
+            Log.LogMessage("Loading " + MODNAME);
             Configuration.CreateAndBindAll();
             new Harmony(MODNAME).PatchAll(typeof(CrosshairPatches));
-            Logger.LogMessage("Loaded " + MODNAME);
+            Log.LogMessage("Loaded " + MODNAME);
         }
     }
 }
